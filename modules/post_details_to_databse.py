c348a711542cffe5af7a57940b6b0210316c854c
Admin <admin@Intern2-Macbook-Pro.local>
2019-02-18 20:20:49 +0530

    Add image for Welcome_page

---
+++ modules/post_details_to_databse.py
@@ -0,0 +1,6 @@
+def post_details(connection, user_data):
+    cursor = connection.cursor()
+    cursor.execute("""insert into student_details(student_id,student_name) values(%s, %s);""",
+                   (user_data['id'], user_data['name']))
+    connection.commit()
+    cursor.close()
