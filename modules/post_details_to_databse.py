8f8958a98ed886f125f3f289c128ada84f098152
Admin <admin@Intern2-Macbook-Pro.local>
2019-02-22 10:50:31 +0530

    Add user entered data to database

--- modules/post_details_to_databse.py
+++ modules/post_details_to_databse.py
@@ -1,9 +1,6 @@
 def post_data(connection, user_data):
     cursor = connection.cursor()
-    cursor.execute("""SELECT * INTO thoughtworks_cafeteria FROM employee_details WHERE employee_id==employeeid;
-                        IF NOT FOUND THEN 
-                        RAISE EXCEPTION 'employee % not found',employeeid
-                        END IF;""",
-                   (user_data['employeeid']))
+    cursor.execute("""insert into employee_details(employee_id) values(%s);""",
+                   (user_data['employeeid'],))
     connection.commit()
     cursor.close()
