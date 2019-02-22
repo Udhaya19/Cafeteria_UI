5825a756b923ae9eb6cb998aaf9fa0aefc8415a0
Admin <admin@Intern2-Macbook-Pro.local>
2019-02-22 14:21:42 +0530

    Add the validation function for employeeid

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import psycopg2
 from flask import Flask, request, render_template
 from flask_cors import CORS
-
 from modules.post_details_to_databse import post_data
 
 app = Flask(__name__)
@@ -19,21 +18,25 @@ def employee_login():
     return render_template('employee_id.html')
 
 
+def validate_employee(connection, user_data):
+    cursor = connection.cursor()
+    result = cursor.execute("""select employee_id from employee_details where employee_id = %s ;""",
+                            (user_data['employeeid'],))
+    connection.commit()
+    cursor.close()
+    if result == user_data['employeeid']:
+        return render_template('beverage_type.html')
+    else:
+        return render_template('employee_id.html')
+
+@app.route('/beverage', methods=['POST'])
+def beverage_type():
+    post_data(connection, request.form)
+    return render_template('beverage_type.html', shared=request.form)
 @app.route('/vendorloginpage')
 def vendor_login():
     return render_template('vendor_login.html')
 
-
-@app.route('/beverage',methods=['POST'])
-def beverage_type():
-    post_data(connection, request.form)
-    return render_template('student_details.html', shared=request.form)
-
-
-# @app.route('/post-data', methods=['POST'])
-# def get_data():
-#     post_data(connection, request.form)
-#     return render_template('student_details.html', shared=request.form)
 
 
 if __name__ == '__main__':
